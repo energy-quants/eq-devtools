daef18dd78e1dc66a43fec6741e6c15d5b2ebe46
Dave Hirschfeld <dave.hirschfeld@gmail.com>
2023-07-17 15:39:29 +1000

    Initial scaffolding

---
+++ src/eq/devtools/cli/_cli.py
@@ -0,0 +1,20 @@
+import click
+
+from .. import __version__
+
+
+@click.group()
+@click.version_option(version=__version__)
+def cli():
+    pass
+
+
+@cli.command()
+@click.option(
+    "--name",
+    help="The name of the person to greet.",
+    type=click.STRING,
+)
+def greet(name: str):
+    click.echo(f"Hello {name}!")
+
