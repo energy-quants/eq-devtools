8026d16e98f27ce5631356f242e4de4072754bc8
Dave Hirschfeld <dave.hirschfeld@gmail.com>
2023-08-28 22:58:40 +1000

    Update CI Scripts (#6)

--- src/eq/devtools/conda/_publish_oci.py
+++ src/eq/devtools/conda/_publish_oci.py
@@ -23,7 +23,10 @@ def publish_oci_artifact(
     FIXME!
 
     """
-    token = token or os.environ["GITHUB_TOKEN"]
+    token = token or os.environ.get("GHA_PAT", default=os.environ.get("GITHUB_TOKEN"))
+    if token is None:
+        msg = "The `GITHUB_TOKEN` environment variable needs to be set!"
+        raise RuntimeError(msg)
     os.environ["GHA_PAT"] = token
     os.environ["GHA_USER"] = owner
     if debug:
